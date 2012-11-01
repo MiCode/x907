1bb1fd46fa86c8b1573aa04433111ec229637ea8
mochangming <mochangming@xiaomi.com>
2012-11-01 14:37:56 +0800

    fix build error: method index too large

--- customize_framework.sh
+++ customize_framework.sh
@@ -6,19 +6,15 @@ APKTOOL="other/tools/apktool"
 
 if [ $2 = "out/framework" ]
 then
-    echo "delete framework redundance files"
-    for file2 in `find oppoframework.jar.out -name *.smali`; do
-            file=${file2/oppoframework.jar.out/out\/framework}
-            echo "rm file: $file"
-            rm -rf "$file"
-    done
-
     echo "make oppoframework.jar"
+
+    cp -r "oppoframework.jar.out" "out/oppoframework"
+    mv "out/framework/smali/miui" "out/oppoframework/smali"
+    mkdir -p "out/oppoframework/smali/android"
+    mv "out/framework/smali/android/widget" "out/oppoframework/smali/android"
+
+    $APKTOOL b  "out/oppoframework" "out/oppoframework.jar"
     mkdir -p "out/ZIP/system/framework"
-    cp -r "oppoframework.jar.out" "out/"
-    mv "out/framework/smali/miui" "out/oppoframework.jar.out/smali"
-    
-    $APKTOOL b  "out/oppoframework.jar.out" "out/oppoframework.jar"
     cp "out/oppoframework.jar" "out/ZIP/system/framework/oppoframework.jar"
 
 elif [ $2 = "out/android.policy" ];then
